cfd28930cc33cb97dd216b4cba8bb69dac28f9ea
fc <1018555611@qq.com>
2020-10-19 17:08:23 +0800

    添加人脸抓拍

---
+++ src/view/FaceView.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Table, Button, Tag } from 'antd';
+import moment from 'moment';
+import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons';
+import HttpApi from '../util/HttpApi';
+function queenChange(list) {
+    let copyList = JSON.parse(JSON.stringify(list))
+    copyList.forEach((item, index) => {
+        if (index % 2 === 1) {
+            copyList[index - 1]['next'] = item
+        }
+    })
+    let afterFilter = copyList.filter((item) => {
+        return item['next']
+    })
+    if (copyList.length % 2 === 1) {
+        afterFilter = [...afterFilter, copyList[copyList.length - 1]]
+    }
+    return afterFilter
+}
+
+export default _ => {
+    const [isFullScreen, setIsFullScreen] = useState(document.isFullScreen || document.webkitIsFullScreen)
+    const [data, setData] = useState([])
+    const [timeStr, setTimeStr] = useState('')
+    const columns = [{
+        title: '时间', width: 100, dataIndex: 'createdAt', key: 'createdAt', render: (_, record) => {
+            return <span style={{ color: '#1890ff' }}>{moment(record.createdAt).format('HH:mm:ss')}</span>
+        }
+    },
+    {
+        title: '照片', dataIndex: 'fid', key: 'fid', render: (_, record) => {
+            const imgUrl = 'https://xiaomei-face.oss-cn-hangzhou.aliyuncs.com/' + record.gid + '/' + record.sid + '/' + record.did + '/' + record.uid + '/' + record.fid + '.png'
+            return <img style={{ width: 50, height: 50 }} src={imgUrl} alt='' />
+        }
+    },
+    {
+        title: '时间', width: 100, dataIndex: 'createdAt_next', key: 'createdAt_next', render: (_, record) => {
+            return record.next ? <span style={{ color: '#1890ff' }}>{moment(record.next.createdAt).format('HH:mm:ss')}</span> : ''
+        }
+    },
+    {
+        title: '照片', dataIndex: 'next', key: 'fid_next', render: (_, record) => {
+            let imgUrl = 'https://xiaomei-face.oss-cn-hangzhou.aliyuncs.com/' + record.gid + '/' + record.sid + '/' + record.did + '/' + record.uid + '/' + record.fid + '.png'
+            if (record.next) {
+                imgUrl = 'https://xiaomei-face.oss-cn-hangzhou.aliyuncs.com/' + record.next.gid + '/' + record.next.sid + '/' + record.next.did + '/' + record.next.uid + '/' + record.next.fid + '.png'
+            }
+            return record.next ? <img style={{ width: 50, height: 50 }} src={imgUrl} alt='' /> : ''
+        }
+    },
+    ]
+    const handleFullScreen = useCallback(() => {
+        let element = document.documentElement
+        if (isFullScreen) {
+            if (document.exitFullscreen) {
+                document.exitFullscreen()
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen()
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen()
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen()
+            }
+        } else {
+            if (element.requestFullscreen) {
+                element.requestFullscreen()
+            } else if (element.webkitRequestFullScreen) {
+                element.webkitRequestFullScreen()
+            } else if (element.mozRequestFullScreen) {
+                element.mozRequestFullScreen()
+            } else if (element.msRequestFullscreen) {
+                element.msRequestFullscreen()
+            }
+        }
+    }, [isFullScreen])
+    const getFaceData = useCallback(async () => {
+        let startOfDay = moment().startOf('day').format('YYYY-MM-DD HH:mm:ss')
+        let endOfDay = moment().endOf('day').format('YYYY-MM-DD HH:mm:ss')
+
+        let sql = `select * from faces where createdAt>='${startOfDay}' and createdAt<='${endOfDay}' order by id desc limit 20`
+        let result = await HttpApi.obs({ sql })
+        if (result.data.code === 0) {
+            let afterQueen = queenChange(result.data.data.map((item, index) => { item.key = index; return item }))
+            setData(afterQueen)
+        }
+    }, [])
+    useEffect(() => {
+        window.onresize = () => {
+            setIsFullScreen(document.isFullScreen || document.webkitIsFullScreen)
+        }
+        let loop = setInterval(() => {
+            setTimeStr(moment().format('YYYY-MM-DD HH:mm:ss'))
+        }, 1000)
+        getFaceData()
+        let loop2 = setInterval(() => {
+            getFaceData()
+        }, 1000)
+        return () => {
+            clearInterval(loop)
+            clearInterval(loop2)
+        }
+    }, [getFaceData])
+    return <div style={styles.root}>
+        <Table
+            title={() => <div style={styles.title}><h2>今日到访</h2><span><Tag color='#85a5ff' style={{ fontSize: 20 }}>{timeStr}</Tag>
+                <Button style={styles.icon} icon={!isFullScreen ? <FullscreenOutlined /> : <FullscreenExitOutlined />} type="ghost" onClick={() => {
+                    handleFullScreen()
+                }}></Button></span></div>}
+            bordered
+            columns={columns}
+            dataSource={data}
+            pagination={false}
+        />
+    </div>
+}
+const styles = {
+    root: {
+        width: '100%',
+        height: '100vh',
+        // backgroundColor: '#FFFFFF',
+        // padding: 24
+    },
+    titleTime: {
+        marginLeft: 20,
+        color: '#1890ff'
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    icon: {
+        marginLeft: 20
+    }
+}
--- src/view/MainView.jsx
+++ src/view/MainView.jsx
@@ -1,42 +1,17 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import HttpApi from '../util/HttpApi';
 import AppleInfoPanel from './AppleInfoPanel';
-import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons'
 import '../css/demo.css'
 import '../css/normalize.css'
 import '../css/style.css'
-import { Button } from 'antd';
+import { Row, Col } from 'antd';
+import FaceView from './FaceView';
 let lastCode = '';
 export default _ => {
-    const [isFullScreen, setIsFullScreen] = useState(document.isFullScreen || document.webkitIsFullScreen)
     const [data, setData] = useState({})
     const [visible, setVisible] = useState(false)
-    const handleFullScreen = useCallback(() => {
-        let element = document.documentElement
-        if (isFullScreen) {
-            if (document.exitFullscreen) {
-                document.exitFullscreen()
-            } else if (document.webkitCancelFullScreen) {
-                document.webkitCancelFullScreen()
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen()
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen()
-            }
-        } else {
-            if (element.requestFullscreen) {
-                element.requestFullscreen()
-            } else if (element.webkitRequestFullScreen) {
-                element.webkitRequestFullScreen()
-            } else if (element.mozRequestFullScreen) {
-                element.mozRequestFullScreen()
-            } else if (element.msRequestFullscreen) {
-                element.msRequestFullscreen()
-            }
-        }
-    }, [isFullScreen])
+
     const getData = useCallback(async () => {
-        console.log('getData')
         let sql = `select t1.*,group_concat(u_m_j.mj_id) as major_id_all,group_concat(majors.name) as major_name_all from
         (select orders.*,users.name,users.permission,users.username,
         levels.name as level_name from order_search_list 
@@ -54,14 +29,14 @@ export default _ => {
             // console.log('查询的code:', result.data.data[0].code)
             // console.log('上次的code:', lastCode)
             if (result.data.data[0].code !== lastCode) {
-                console.log('新的搜索记录；展示')
+                console.log('order新的搜索记录；展示')
                 lastCode = result.data.data[0].code
                 setData(result.data.data[0])
                 setVisible(true)
             } else {
-                console.log('老的搜索记录；不展示')
+                console.log('order老的搜索记录；不展示')
             }
-        } else { console.log('没有最新数据'); setVisible(false); lastCode = ''; }
+        } else { console.log('order没有最新数据'); setVisible(false); lastCode = ''; }
     }, [])
     const updateHandler = useCallback(async () => {
         let sql = `update order_search_list set is_read = 1 where order_code = ${lastCode}`
@@ -69,9 +44,6 @@ export default _ => {
         lastCode = ''
     }, [])
     useEffect(() => {
-        window.onresize = () => {
-            setIsFullScreen(document.isFullScreen || document.webkitIsFullScreen)
-        }
         setInterval(() => {
             getData()
         }, 5000)
@@ -79,18 +51,21 @@ export default _ => {
     return <div style={styles.root}>
         <AppleInfoPanel data={data} visible={visible} onClose={() => { setVisible(false); updateHandler() }} />
         <div className="satic-area">
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <div style={styles.icon}>
-                    <div style={styles.icon} id="weather-v2-plugin-standard"></div>
-                </div>
-                <Button icon={!isFullScreen ? <FullscreenOutlined style={styles.icon} /> : <FullscreenExitOutlined style={styles.icon} />} style={{ zIndex: 10 }} type="ghost" onClick={() => {
-                    handleFullScreen()
-                }}></Button>
-            </div>
             <div className="dynamic-area1"></div>
             <div className="dynamic-area2"></div>
+            <div>
+                <Row>
+                    <Col span={16}>
+                        <div id="weather-v2-plugin-standard"></div>
+                    </Col>
+                    <Col span={8}>
+                        <div><FaceView /></div>
+                    </Col>
+                </Row>
+            </div>
         </div>
-    </div >
+
+    </div>
 }
 const styles = {
     root: {
@@ -98,11 +73,4 @@ const styles = {
         height: '100vh',
         backgroundColor: '#b5f5ec'
     },
-    icon: {
-        zIndex: 10
-    },
-    timefont: {
-        fontSize: 24,
-        color: '#8c8c8c'
-    }
 }
